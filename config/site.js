f060a3e124dd0bc760ea8d66e4166eaf44f9cca6
YonatanBenezra <yonatanbenezra1@gmail.com>
2021-11-28 14:40:37 +0200

    site for G2910

---
+++ config/site.js
@@ -0,0 +1,25 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: 'G2910', // Navigation and Site Title
+  siteTitleAlt: 'G2910 - Photography', // Alternative Site title for SEO
+  siteTitleShort: 'G2910', // short_name for manifest
+  siteHeadline: 'G2910', // Headline for schema.org JSONLD
+  siteUrl: 'https://G2910.netlify.com', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/logos/logo.png', // Used for SEO and manifest
+  siteDescription: 'G2910 - Personal photography portfolio',
+  author: 'G2910', // Author for schema.org JSONLD
+  ogLanguage: 'en_US', // og:language
+  googleAnalyticsID: 'UA-134925752-3',
+
+  // Manifest and Progress color
+  themeColor: '#3498DB',
+  backgroundColor: '#2b2e3c',
+
+  // Your information
+  avatar: '/logos/logo.png',
+  name: 'G2910',
+  location: 'United states',
+  socialMedia: [],
+}
